e4ce8ae49af40f0e241a7a0b8581259128514126
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-07-24 22:37:02 +0530

    Ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,17 @@
+import os
+from flask import Flask
+from app.routes import register_routes
+
+def create_app():
+    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+    app = Flask(__name__, template_folder=template_dir)
+    
+    # Load secret key from environment
+    app.secret_key = os.environ.get('SECRET_KEY', 'fallback-key-for-local-dev')
+
+    register_routes(app)
+    return app
+
 # import os
 # from flask import Flask
 # from app.routes import register_routes
@@ -10,19 +24,3 @@
 #     app = Flask(__name__, template_folder=template_dir)
 #     register_routes(app)
 #     return app
-
-import os
-from flask import Flask
-from app.routes import register_routes
-
-def create_app():
-    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
-    app = Flask(__name__, template_folder=template_dir)
-    
-    # SECRET_KEY='fc7e97116ab861918317c2c3c11c049f243ef7e77703488b35f80c9ca88cdcdc'
-
-    # Load secret key from environment
-    app.secret_key = os.environ.get('SECRET_KEY', 'fallback-key-for-local-dev')
-
-    register_routes(app)
-    return app
